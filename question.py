973deb8a19dde5af1fd822e0f051e6bae503025d
Kandhy-0X9 <kashengkandhy@gmail.com>
2026-01-12 12:54:04 -0500

    crane

--- question.py
+++ question.py
@@ -1,6 +1,21 @@
 #Questionnaire
+#imports
+import os
+#clear terminal before program starts
+def clearTerminal():
+    os.system('cls||clear')
 
+##Show title and greet the user
+##Show the user different passages to read from
+##Give the user questions based on the chosen passage
+##Check the answers and provide corrections
+##Loop the program
 #main function
 def main():
-    print("QUESTIONNAIRE")
-    print("")
+    clearTerminal()
+    ##Show title and greet the user
+    print("BIRDS QUESTIONNAIRE\n")
+    print("\nWelcome to birds questionnaire\n\n")
+    ##Show the user different passages to read from
+    print("Birds:\n")
+    print("Flamingo Columbidae Cassowaries Cranes Albatross")
