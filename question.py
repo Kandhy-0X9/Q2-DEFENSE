63d0b84324979b7b2e676e19a0ea3a64a06f2445
Kandhy-0X9 <kashengkandhy@gmail.com>
2026-01-12 13:28:26 -0500

    drdrhreh

--- question.py
+++ question.py
@@ -2,20 +2,20 @@
 #imports
 import os
 #clear terminal before program starts
-def clearTerminal():
-    os.system('cls||clear')
+# def clearTerminal():
+#     os.system('cls||clear')
 
-##Show title and greet the user
-##Show the user different passages to read from
-##Give the user questions based on the chosen passage
-##Check the answers and provide corrections
-##Loop the program
-#main function
-def main():
-    clearTerminal()
-    ##Show title and greet the user
-    print("BIRDS QUESTIONNAIRE\n")
-    print("\nWelcome to birds questionnaire\n\n")
-    ##Show the user different passages to read from
-    print("Birds:\n")
-    print("Flamingo Columbidae Cassowaries Cranes Albatross")
+# ##Show title and greet the user
+# ##Show the user different passages to read from
+# ##Give the user questions based on the chosen passage
+# ##Check the answers and provide corrections
+# ##Loop the program
+# #main function
+# def main():
+#     clearTerminal()
+#     ##Show title and greet the user
+#     print("BIRDS QUESTIONNAIRE\n")
+#     print("\nWelcome to birds questionnaire\n\n")
+#     ##Show the user different passages to read from
+#     print("Birds:\n")
+#     print("Flamingo Columbidae Cassowaries Cranes Albatross")
